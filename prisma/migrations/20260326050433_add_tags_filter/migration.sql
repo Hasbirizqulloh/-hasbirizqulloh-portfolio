9dc9a8b95e75340795492b9004bdd3af30e48819
Moh Hasbi Rizqulloh <hasbirizqulloh95@gmail.com>
2026-03-26 12:07:13 +0700

    add migration prisma add_tags_filter

---
+++ prisma/migrations/20260326050433_add_tags_filter/migration.sql
@@ -0,0 +1,85 @@
+-- CreateEnum
+CREATE TYPE "MessageStatus" AS ENUM ('UNREAD', 'READ', 'RESPONDED');
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "coverImageUrl" TEXT,
+    "liveDemoUrl" TEXT,
+    "sourceCodeUrl" TEXT,
+    "architectureDetails" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Technology" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "iconType" TEXT,
+
+    CONSTRAINT "Technology_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectTechnology" (
+    "projectId" TEXT NOT NULL,
+    "technologyId" TEXT NOT NULL,
+
+    CONSTRAINT "ProjectTechnology_pkey" PRIMARY KEY ("projectId","technologyId")
+);
+
+-- CreateTable
+CREATE TABLE "BlogPost" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "excerpt" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "coverImageUrl" TEXT,
+    "readTimeMinutes" INTEGER NOT NULL DEFAULT 5,
+    "author" TEXT NOT NULL DEFAULT 'Hasbirizqulloh',
+    "isFeatured" BOOLEAN NOT NULL DEFAULT false,
+    "tags" TEXT[],
+    "publishedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BlogPost_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ContactMessage" (
+    "id" TEXT NOT NULL,
+    "senderName" TEXT NOT NULL,
+    "senderEmail" TEXT NOT NULL,
+    "subject" TEXT,
+    "message" TEXT NOT NULL,
+    "status" "MessageStatus" NOT NULL DEFAULT 'UNREAD',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ContactMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Technology_name_key" ON "Technology"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "BlogPost_slug_key" ON "BlogPost"("slug");
+
+-- AddForeignKey
+ALTER TABLE "ProjectTechnology" ADD CONSTRAINT "ProjectTechnology_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectTechnology" ADD CONSTRAINT "ProjectTechnology_technologyId_fkey" FOREIGN KEY ("technologyId") REFERENCES "Technology"("id") ON DELETE CASCADE ON UPDATE CASCADE;
